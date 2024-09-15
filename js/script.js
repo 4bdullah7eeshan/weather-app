2bdd8507504f75cedc693595830c24eae0163c83
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-16 05:11:43 +0530

    feat: display weather data as per user's input

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const apiKey = '?key=JVADF28JJP4RBZB6GEU9W4MSZ';
 
 let url;
 
-const submitButton = document.querySelector("button");
+const submitButton = document.querySelector("#user");
 submitButton.addEventListener("submit", displayWeatherData);
 
 async function getApiWeatherData () {
@@ -29,18 +29,40 @@ async function extractRequiredAppData () {
 }
 
 
-async function displayWeatherData() {
-    const userLocation = document.querySelector("#location").value.toLowerCase();
+async function displayWeatherData(e) {
+    e.preventDefault();
+
+    const userLocation = document.querySelector("#city").value.toLowerCase();
 
     url = baseUrl + userLocation + apiKey;
     console.log(userLocation);
     console.log(url);
 
     const weatherDataDiv = document.querySelector(".weather");
+    weatherDataDiv.innerHTML = '';
     let data = await extractRequiredAppData();
-    const c = document.createElement('p');
-    c.textContent = data.city;
-    console.log(data.city);
-    weatherDataDiv.appendChild(c);
+    
+    const cityElement = document.createElement('h2');
+    cityElement.textContent = data.city;
+    weatherDataDiv.appendChild(cityElement);
+
+
+    const temperatureElement = document.createElement('h3');
+    temperatureElement.textContent = data.temperature + 'F';
+    weatherDataDiv.appendChild(temperatureElement);
+
+
+    const weatherConditionElement = document.createElement('p');
+    weatherConditionElement.textContent = data.weatherCondition;
+    weatherDataDiv.appendChild(weatherConditionElement);
+
+
+    const humidityElement = document.createElement('p');
+    humidityElement.textContent = 'Humidity: ' + data.humidity + '%';
+    weatherDataDiv.appendChild(humidityElement);
+
+    const expectationsElement = document.createElement('p');
+    expectationsElement.textContent = data.expectations;
+    weatherDataDiv.appendChild(expectationsElement);
     
 }
