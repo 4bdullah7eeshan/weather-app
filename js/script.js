482f653add33bc497b9c9a6c823b3fb3945058a2
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-16 03:02:35 +0530

    wip: separate api fetching and json processing fns

--- js/script.js
+++ js/script.js
@@ -1,27 +1,30 @@
-let city, temperature, weatherCondition, date, time, humidity, expectations, gif; 
+//let city, temperature, weatherCondition, date, time, humidity, expectations, gif; 
 
 url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=JVADF28JJP4RBZB6GEU9W4MSZ';
 
-async function getWeatherData () {
+async function getApiWeatherData () {
     const response = await fetch(url, {mode: 'cors'});
-    const weatherData = await response.json();
+    const apiWeatherData = await response.json();
 
-    city = weatherData.resolvedAddress;
-    expectations = weatherData.description;
-    temperature = weatherData.currentConditions.temp;
-    weatherCondition = weatherData.currentConditions.conditions;
-    date = weatherData.days[0].datetime;
-    time = weatherData.currentConditions.datetime;
-    humidity = weatherData.currentConditions.humidity;
-    gif = weatherData.currentConditions.icon;
-
-    console.log(city);
-    console.log(date);
-    console.log(time);
-    console.log(temperature);
-    console.log(weatherCondition);
-    console.log(expectations);
-    console.log(gif);
+    return apiWeatherData;
 }
 
-getWeatherData();
+async function extractRequiredAppData () {
+    const jsonData = await getApiWeatherData();
+
+    let current = {
+        city: jsonData.resolvedAddress,
+        expectations: jsonData.description,
+        temperature: jsonData.currentConditions.temp,
+        weatherCondition: jsonData.currentConditions.conditions,
+        date: jsonData.days[0].datetime,
+        time: jsonData.currentConditions.datetime,
+        humidity: jsonData.currentConditions.humidity,
+        gif: jsonData.currentConditions.icon
+    };
+
+    return current;
+}
+
+//console.log(getApiWeatherData());
+console.log(extractRequiredAppData());
